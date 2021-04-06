071a9bac6275756eabe1a1574d54ed2139dc0de1
ItsMeEmmant <81886755+ItsMeEmmant@users.noreply.github.com>
2021-04-06 15:16:22 -0500

    Nuevos comandos, la búsqueda en yt aún no sirve

--- index.py
+++ index.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands
+import datetime
+
+from urllib import parse, request
+import re
+
 
 bot = commands.Bot(command_prefix='*')
 
@@ -12,14 +17,35 @@ async def sum(ctx, numOne: int, numTwo: int):
     await ctx.send(numOne + numTwo)
 
 @bot.command()
+async def info(ctx):
+    embed = discord.Embed(title=f"{ctx.guild.name}", description="Esta es la información de tu server", timestamp=datetime.datetime.utcnow(), color=discord.Color.purple())
+    embed.add_field(name="Fecha de creación", value=f"{ctx.guild.created_at}")
+    embed.add_field(name="Dueño sel server", value=f"{ctx.guild.owner}")
+    embed.add_field(name="Región", value=f"{ctx.guild.region}")
+    embed.add_field(name="ID del Server", value=f"{ctx.guild.id}")
+    embed.set_thumbnail(url="https://a.ppy.sh/9886340")
+
+    await ctx.send(embed=embed)
+    
+@bot.command()
 async def love(ctx, nombre1: str, nombre2: str): 
     await ctx.send(nombre1 + " y " + nombre2 + " se aman mucho OwO" )
+
+@bot.command()
+async def youtube(ctx, *, search):
+    query_string = parse.urlencode({'search_query': search})
+    html = request.urlopen('http://www.youtube.com/results?' + query_string)
+    search_results = re.findall('href=\"\\/watch\\?v=(.{11})', html.read().decode())
+    print(search_results)
+    await ctx.send('https://www.youtube.com/watch?v=' + search_results[0])
+
 # Events
 
 @bot.event
 
 async def on_ready():
     print('Que quieres puta')
+
 
 
 bot.run('ODI4NDkxODkxNzg3MDM4NzQx.YGqXMw.IMVOllN1ScWWbIC15TkDJmkEM1M')
